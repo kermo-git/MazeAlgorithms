815b23d17a754eb427f226045703ae67f5f53a4d
Kermo Saarse <saarse27@gmail.com>
2022-12-05 16:22:14 +0200

    Maze rendering example

---
+++ src/Components/App.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+
+import { initializeMaze, openWall, cellAt } from "../Algorithms/Maze"
+import { MazeDisplay } from "./MazeDisplay"
+
+import "./App.css"
+
+export function App() {
+    // TODO:
+    // Dropdown menu for selecting algorithm
+    // Inputs for maze dimensions
+    // Button to advance one step in the algorithm
+
+    return (<MazeDisplay maze={exampleMaze()}/>)
+}
+
+function exampleMaze() {
+    const maze = initializeMaze(3, 3)
+
+    openWall(maze, {X: 0, Y: 0}, "north")
+    openWall(maze, {X: 0, Y: 1}, "north")
+    openWall(maze, {X: 0, Y: 2}, "east")
+    openWall(maze, {X: 1, Y: 2}, "east")
+    openWall(maze, {X: 2, Y: 2}, "south")
+    openWall(maze, {X: 2, Y: 1}, "south")
+    openWall(maze, {X: 2, Y: 0}, "west")
+    openWall(maze, {X: 1, Y: 0}, "north")
+    
+    cellAt(maze, {X: 1, Y: 1}).color = "lightblue"
+    cellAt(maze, {X: 0, Y: 2}).color = "gold"
+
+    return maze
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
 import * as React from "react"
-import { useState, useRef, useEffect } from "react"
 import { createRoot } from "react-dom/client"
-import "./style.css"
 
-function App() {
-    return (<>
-        <h1>Hello World!</h1>
-    </>)
-}
+import { App } from "./Components/App"
+
 const element = document.createElement("div")
 document.body.appendChild(element)
 createRoot(element).render(<App/>)
